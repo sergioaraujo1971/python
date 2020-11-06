e057fef1735eafa902df153d2dd46fc8454703c6
sergioaraujo1971 <saaraujo@msn.com>
2020-11-06 20:19:08 -0300

    segundo commit

---
+++ listas/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='lista',
+    version='1.0.0',
+    py_modules=['listas'],
+    author='Sergio A Araujo',
+    author_email='saaraujo@msn.com',
+    url='https://github.com/sergioaraujo1971',
+    description='programa de execução de listas de filmes favoritos'
+)
